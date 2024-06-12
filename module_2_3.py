5c4d3b265b53982b2e4bd17f51ed0e76da72a46c
Константин М <knetszu@gmail.com>
2024-06-13 00:00:15 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+index = 0
+while index < len(my_list):
+    if my_list[index] >= a:
+        if(my_list[index]) > a:
+            print(my_list[index])
+        index += 1
+        continue
+    break
+
+
+
